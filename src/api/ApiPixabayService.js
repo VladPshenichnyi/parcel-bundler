6d1fc4e2a8f34f8665ac2e16c2179cc2ac18f2c4
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-02-17 21:40:31 +0200

    step by step

---
+++ src/api/ApiPixabayService.js
@@ -0,0 +1,39 @@
+const API_KEY = '/?key=25756653-ca7b891a55f16e964dd1f6216';
+const BASE_URL = 'https://pixabay.com/api';
+
+// fetch(`${ API_URL }${API_KEY}}&q=${onSearch}`).getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
+
+export default class FetchApiPictures { 
+    constructor() { 
+        this.picture = '';
+        this.seekOptions = 'image_type=photo,orientation=horizontal,safesearch=false';
+    };
+
+    fetchPicture() {
+        const url = `${ BASE_URL }${API_KEY}&q=${this.picture}&${this.seekOptions}`;
+        // console.log(`Мы ищем ==> ${}`);
+        return fetch(url).then((response) => {
+            if (!response.ok) {
+            throw new Error(response.status);
+            }
+            return response.json();
+        })
+            .then(picture => { 
+            // console.log(picture)
+            return picture;                   
+            })
+    }
+
+    get country() { 
+        return this.picture;
+    }
+
+    set country(newPicture) {
+        this.picture = newPicture;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+import FetchApiPictures from './api/ApiPixabayService'
+
+
+let picturesSetFind = '';
+
+const inputRef = document.querySelector('input');
+const resultOfSearch = {
+    picturesList: document.querySelector('.gallery'),
+};
+
+const fetchApiPictures = new FetchApiPictures();
+
+
+inputRef.addEventListener('input', findPictures);
+
+
+// const API_KEY = '/?key=25756653-ca7b891a55f16e964dd1f6216';
+// const BASE_URL = 'https://pixabay.com/api';
+// const url = `${ BASE_URL }${API_KEY}    `
+// fetch(url).then((response) => {
+//             if (!response.ok) {
+//             throw new Error(response.status);
+//             }
+//             return response.json();
+//         }).then(console.log(response))
+
+function findPictures(e) {
+    e.preventDefault()
+    picturesSetFind = inputRef.value.trim();
+    console.log(picturesSetFind);
+}
